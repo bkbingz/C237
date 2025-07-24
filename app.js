829a91c98c2287826e6824f25275c49262923d6b
bkbingz <90766464+bkbingz@users.noreply.github.com>
2025-07-24 15:05:16 +0800

    update sql

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const connection = mysql.createConnection({
+    host: 'l8259z.h.filess.io',
+    user: 'C237DatabaseTeam4_important',
+    password: '12c7924fce1143c2b7153cd4d02af4a9bdae651a',
+    database: 'C237DatabaseTeam4_important'
+  });
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
 req.session.user = {
   username: 'admin',
   role: 'admin' // or 'user'
